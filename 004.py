0d17d51d2fb7325302353e651ace495033b5b6df
NikolayD <kolik885@ya.ru>
2022-10-11 22:04:16 +0700

    Программа №4 урок 4 изменен replace

--- 004.py
+++ 004.py
@@ -30,12 +30,12 @@ def createEquation():
 
 
 polynomial1 = createEquation()
-print((polynomial1.replace('x^1', 'x').replace('x^0', '')).replace('1x^', 'x^'))
+print((polynomial1.replace('x^1 ', 'x ').replace('x^0', '')).replace(' 1x^', ' x^'))
 polynomial2 = createEquation()
-print((polynomial2.replace('x^1', 'x').replace('x^0', '')).replace('1x^', 'x^'))
+print((polynomial2.replace('x^1 ', 'x ').replace('x^0', '')).replace(' 1x^', ' x^'))
 
 with open(r'polynomial.txt', 'w') as data:
-    data.write(str(polynomial1.replace('x^1', 'x').replace('x^0', '')).replace('1x^', 'x^'))
+    data.write(str(polynomial1.replace('x^1 ', 'x ').replace('x^0', '.0')).replace(' 1x^', ' x^'))
     data.write('\n')
-    data.write(str(polynomial2.replace('x^1', 'x').replace('x^0', '')).replace('1x^', 'x^'))
+    data.write(str(polynomial2.replace('x^1 ', 'x ').replace('x^0', '.0')).replace(' 1x^', ' x^'))
     data.close
